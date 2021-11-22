1e9c61776e4dfb4793b82595545d828ab1365cf4
Emily-Waters <emilyabigailwaters@gmail.com>
2021-11-22 23:41:57 +0000

    refactor

--- tail.js
+++ tail.js
@@ -21,4 +21,3 @@ const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
 assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
