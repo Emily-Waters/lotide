ec9b182f2cff567cddfed4067fbe0bd022d1eb41
Emily-Waters <emilyabigailwaters@gmail.com>
2021-11-24 23:27:20 +0000

    Added letterPositions.js

--- countLetters.js
+++ countLetters.js
@@ -50,17 +50,11 @@ const countLetters = (inputString) => {
 };
 
 
-console.log(countLetters('lighthouse in the house'));
-console.log({
-  l: 1,
-  i: 2,
-  g: 1,
-  h: 4,
-  t: 2,
-  o: 2,
-  u: 2,
-  s: 2,
-  e: 3,
-  n: 1,
-});
+//TESTING
+const expectedOutput = {l: 1,i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1};
+const actualOutput = countLetters('lighthouse in the house');
+
+for (let key in actualOutput) {
+  assertEqual(actualOutput[key], expectedOutput[key]);
+}
 
---
+++ letterPositions.js
@@ -0,0 +1,80 @@
+const assertArraysEqual = (actual,expected) => {
+
+  if (eqArrays(actual,expected)) {
+
+    console.log('💚️\tAssertion Passed:', actual ,'===', expected);
+  
+  } else {
+
+    console.log('🔴️\tAssertion Failed:', actual ,'!==', expected);
+
+  }
+};
+
+const eqArrays = (actual,expected) => {
+
+  if (actual.length !== expected.length) {
+
+    return false;
+
+  }
+
+
+  for (let index = 0; index <= actual.length - 1; index++) {
+
+    if (actual[index] !== expected[index]) {
+
+      return false;
+
+    }
+
+  }
+
+  return true;
+};
+
+const letterPositions = (inputStr) => {
+
+  //resultObj stores the contents of indexArr as values with their associated keys
+  const resultObj = {};
+  let indexArr = [];
+
+  for (const letter of inputStr) {
+    
+    indexArr = [];
+
+    //Check if the key does not exist in resultObj so that we can add it or ignore
+    if (!resultObj[letter] && letter !== ' ') {
+
+      //Iterate through the string, providing index values for each letter
+      for (const index in inputStr) {
+
+        //Check if our current letter is equal to the letter in the original string
+        if (letter === inputStr[index]) {
+
+          //Append the index position as a number to the array of indexes
+          indexArr.push(Number(index));
+
+        }
+
+        //Add the letter as a key and the array of indexes as its value
+        resultObj[letter] = indexArr;
+
+      }
+      
+    }
+  }
+
+  return resultObj;
+};
+
+const actualOutput = letterPositions('lighthouse in the house');
+const expectedOutput = {l: [0],i: [1, 11],g: [2],h: [3, 5, 15, 18],t: [4, 14],o: [6, 19],u: [7, 20],s: [8, 21],e: [9, 16, 22],n: [12]};
+
+console.log(actualOutput);
+console.log(expectedOutput);
+
+for (let key in actualOutput) {
+  assertArraysEqual(actualOutput[key], expectedOutput[key]);
+}
+
