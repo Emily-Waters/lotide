25af041dd2bfdd258a870d71da339faebf108a1f
Emily-Waters <emilyabigailwaters@gmail.com>
2021-11-29 21:38:49 +0000

    RF middle for import/export, added test/middleTest.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,32 +1,3 @@
-//  Console will not log anything if the argument in brackets evaluates as true.
-//console.assert(true === true);
-//  Console will log 'Assertion failed' for arguments that evaluate to false.
-//console.assert(true === false);
-
-/* NOTES ON SYNTAX FROM MDN FOR CONSOLE.ASSERT (https://developer.mozilla.org/en-US/docs/Web/API/console/assert)
-* console.assert(assertion, obj1 [, obj2, ..., objN]);
-* console.assert(assertion, msg [, subst1, ..., substN]); // C-like message formatting
-*
-*
-* NOTES FROM COMPASS LHL (https://web.compass.lighthouselabs.ca/days/w01d1/activities/807)
-*
-*    // FUNCTION IMPLEMENTATION
-*     const sum = function(a, b) {
-*      return a + b;
-*    }
-*
-*    // TEST CODE
-*    console.assert(sum(1, 2) === 3);
-*    console.assert(sum(1, 20) === 3); // bad / incorrect assertion, and we see it fail!
-*
-*   The console.assert function prints to the console for us, the developer,
-*   when an expected outcome is not met (fails) so that we can look into why.
-*/
-
-//  Creating my own assertEqual func for testing
-
-
-//IMPORTS
 const assertEqual = (actual,expected) => {
 
   if (actual === expected) {
@@ -36,6 +7,4 @@ const assertEqual = (actual,expected) => {
   }
 };
 
-
-//EXPORTS
 module.exports = assertEqual;
