c242cde34b2e305251b5b9c03d9555f6aa0ac598
Emily-Waters <emilyabigailwaters@gmail.com>
2021-11-29 21:32:53 +0000

    assertArraysEqual now imports/exports, testing functionality moved to test dir

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = (actual,expected) => {
   if (eqArrays(actual,expected)) {
     console.log('💚️💚️💚️\tAssertion Passed:', actual ,'===', expected);
@@ -6,21 +8,4 @@ const assertArraysEqual = (actual,expected) => {
   }
 };
 
-const eqArrays = (actual,expected) => {
-  if (actual.length !== expected.length) {
-    return false;
-  }
-  for (let index = 0; index <= actual.length - 1; index++) {
-    if (actual[index] !== expected[index]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertArraysEqual([5, 2, 3], [1, 2, 3]); // => should FAIL
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertArraysEqual([1, 2, '3'], [1, 2, 3]); // => should FAIL
-assertArraysEqual([1, 2, 3, 4], [1, 2, 3]); // => should FAIL
-
+module.exports = assertArraysEqual;
